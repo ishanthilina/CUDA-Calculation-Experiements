3485544f3a4aa257979999ce6bbc4fea791e369f
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2014-01-16 00:00:45 +0530

    Initial code added

--- q3/q3.cu
+++ q3/q3.cu
@@ -12,3 +12,22 @@ typedef double Real;
 #else
 typedef float Real;
 #endif
+
+
+#define MATRIX_SIZE 12
+
+__global__ void cuda_simple_mat_mul(float* _pA, float* _pB, float* _pC) {
+
+	int col = threadIdx.x + blockIdx.x * blockDim.x;
+	int row = threadIdx.y + blockIdx.y * blockDim.y;
+	
+	//check for bounds
+	if(row < MATRIX_SIZE && col < MATRIX_SIZE)
+	{
+		float sum = 0.f;
+		for (int i = 0; i < MATRIX_SIZE; i++)
+			sum += _pA[row * MATRIX_SIZE + i] * _pB[i * MATRIX_SIZE + col];
+
+		_pC[row * MATRIX_SIZE + col] = sum;
+	}
+}
