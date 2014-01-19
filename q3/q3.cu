69d52dc78ccdf4f3ceeffecc24284fb70b59de25
Ishan Thilina Somasiri <ishanthilina@gmail.com>
2014-01-19 19:02:10 +0530

    Code formatted

--- q3/q3.cu
+++ q3/q3.cu
@@ -15,7 +15,6 @@
 #define MIN_ERROR 0.1
 
 // CUDA related
-// #define THREADS_PER_BLOCK 256
 #define BLOCK_SIZE 32
 
 
@@ -25,7 +24,8 @@ inline void gpuAssert(cudaError_t code, char *file, int line, bool abort=true)
 {
 	if (code != cudaSuccess) 
 	{
-		fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+		fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code),\
+		 file, line);
 		if (abort) exit(code);
 	}
 }
@@ -68,16 +68,25 @@ static unsigned long inMB(unsigned long bytes)
 
  	printf("---------------------------------------------------------------\n");
 
- 	printf("^^^^ Free : %lu bytes (%lu KB) (%lu MB)\n", free, inKB(free), inMB(free));
+ 	printf("^^^^ Free : %lu bytes (%lu KB) (%lu MB)\n", free, inKB(free), \
+ 		inMB(free));
 
- 	printf("^^^^ Total: %lu bytes (%lu KB) (%lu MB)\n", total, inKB(total), inMB(total));
+ 	printf("^^^^ Total: %lu bytes (%lu KB) (%lu MB)\n", total, inKB(total), \
+ 		inMB(total));
 
- 	printf("^^^^ %f%% free, %f%% used\n", 100.0*free/(double)total, 100.0*(total - free)/(double)total);
+ 	printf("^^^^ %f%% free, %f%% used\n", 100.0*free/(double)total, \
+ 		100.0*(total - free)/(double)total);
  	printf("---------------------------------------------------------------\n");
 
  }
 
-
+/**
+ * Carries out a simple square matrix multiplication where each thread
+ * calculates a single item in the resulting matrix.
+ * @param A First matrix
+ * @param B Second matrix
+ * @param C Results matrix
+ */
  __global__ void cuda_simple_mat_mul(Real* A, Real* B, Real* C) {
 
  	int col = threadIdx.x + blockIdx.x * blockDim.x;
@@ -109,20 +118,22 @@ static unsigned long inMB(unsigned long bytes)
  	}
  }
 
+/**
+ * Prints the given matrix to the stdout
+ */
  void print_matrix(Real matrix[MATRIX_DIM][MATRIX_DIM])
  {
 
  	for(int i = 0; i < MATRIX_DIM; i++)
  	{
  		printf("[");
-
  			for(int j  = 0; j < MATRIX_DIM; j++)
  			{
-			#ifdef DP
+		#ifdef DP
  				printf("%20.18f ", matrix[i][j]);
-    		#else
+    	#else
  				printf("%f ", matrix[i][j]);
-    		#endif
+    	#endif
 
  				
  			}
@@ -131,36 +142,44 @@ static unsigned long inMB(unsigned long bytes)
  		printf("\n");
  	}
 
- 	void compare_matrices(Real matrix1[MATRIX_DIM][MATRIX_DIM], Real matrix2[MATRIX_DIM][MATRIX_DIM])
+/**
+ * Compares the given two matrices.
+ */
+ void compare_matrices(Real matrix1[MATRIX_DIM][MATRIX_DIM],\
+ 	Real matrix2[MATRIX_DIM][MATRIX_DIM])
+ {
+ 	for(int i = 0; i < MATRIX_DIM; i++)
  	{
- 		for(int i = 0; i < MATRIX_DIM; i++)
+ 		for(int j = 0; j < MATRIX_DIM; j++)
  		{
- 			for(int j = 0; j < MATRIX_DIM; j++)
+ 			if((matrix1[i][j] - matrix2[i][j] > MIN_ERROR) &&
+ 				(matrix1[i][j] - matrix2[i][j] > 0))
  			{
- 				if((matrix1[i][j] - matrix2[i][j] > MIN_ERROR) &&
- 					(matrix1[i][j] - matrix2[i][j] > 0))
- 				{
- 					printf("Error i=%d : j=%d mat1=%f mat2=%f\n",i,j,matrix1[i][j], matrix2[i][j]);
- 					// return;
- 				}
- 			}
- 		}
-
- 		printf("Matrices Match! \n");
- 	} 
-
- 	void serial_mat_mul(Real A[MATRIX_DIM][MATRIX_DIM], Real B[MATRIX_DIM][MATRIX_DIM], Real C[MATRIX_DIM][MATRIX_DIM])	{
- 		float sum;
- 		for (int row=0; row<MATRIX_DIM; row++){
- 			for (int col=0; col<MATRIX_DIM; col++){
- 				sum = 0.f;
- 				for (int n=0; n<MATRIX_DIM; n++){
- 					sum += A[row][n]*B[n][col];
- 				}
- 				C[row][col] = sum;
+ 				printf("Error i=%d : j=%d mat1=%f mat2=%f\n",i,j,\
+ 					matrix1[i][j], matrix2[i][j]);
+ 				return;
  			}
  		}
  	}
+
+ 	printf("Matrices Match! \n");
+ } 
+/**
+ * carries out a serial matrix multiplication
+ */
+ void serial_mat_mul(Real A[MATRIX_DIM][MATRIX_DIM], \
+ 	Real B[MATRIX_DIM][MATRIX_DIM], Real C[MATRIX_DIM][MATRIX_DIM])	{
+ 	float sum;
+ 	for (int row=0; row<MATRIX_DIM; row++){
+ 		for (int col=0; col<MATRIX_DIM; col++){
+ 			sum = 0.f;
+ 			for (int n=0; n<MATRIX_DIM; n++){
+ 				sum += A[row][n]*B[n][col];
+ 			}
+ 			C[row][col] = sum;
+ 		}
+ 	}
+ }
 
 /**
  * Shows the usage of the program.
@@ -169,28 +188,14 @@ static unsigned long inMB(unsigned long bytes)
  	printf("Wrong usage!\n");
  }
 
+/**
+ * Does a matrix multiplication using the "tiled" approach in the GPU
+ * @param A First matrix
+ * @param B Second matrix
+ * @param C Results matrix
+ */
  __global__ void cuda_tiled_mat_mul(Real * A, Real * B, Real * C) {
- 	// Real CValue = 0;
- 	// if (((blockIdx.y * blockDim.y + threadIdx.y)< MATRIX_DIM) && ((blockIdx.x * blockDim.x + threadIdx.x) < MATRIX_DIM)) {
- 	// 	for (int k = 0; k < (MATRIX_DIM / BLOCK_SIZE); ++k) {
-
- 	// 		Real * ASub =  &A[MATRIX_DIM * BLOCK_SIZE * blockIdx.y + BLOCK_SIZE * k];
- 	// 		Real * BSub = &B[MATRIX_DIM*BLOCK_SIZE*k + BLOCK_SIZE*blockIdx.x];
-
- 	// 		__shared__ Real As[BLOCK_SIZE][BLOCK_SIZE];
- 	// 		__shared__ Real Bs[BLOCK_SIZE][BLOCK_SIZE];
-
- 	// 		As[threadIdx.y][threadIdx.x] = ASub[threadIdx.y*MATRIX_DIM+threadIdx.x];
- 	// 		Bs[threadIdx.y][threadIdx.x] = BSub[threadIdx.y*MATRIX_DIM+threadIdx.x];
-
- 	// 		__syncthreads();
- 	// 		for (int n = 0; n < BLOCK_SIZE; ++n)
- 	// 			CValue += As[threadIdx.y][n] * Bs[n][threadIdx.x];
- 	// 		__syncthreads();
- 	// 	}
- 	// 	C[((blockIdx.y * blockDim.y + threadIdx.y)*MATRIX_DIM)+(blockIdx.x*blockDim.x)+threadIdx.x]=CValue;
- 	// }
- 	// 
+ 	
  	float CValue = 0;
 
  	int Row = blockIdx.y*BLOCK_SIZE + threadIdx.y;
@@ -200,21 +205,41 @@ static unsigned long inMB(unsigned long bytes)
  	__shared__ Real Bs[BLOCK_SIZE][BLOCK_SIZE];
 
  	for (int k = 0; k < (BLOCK_SIZE + MATRIX_DIM - 1)/BLOCK_SIZE; k++) {
+ 		// check ranges for the matrices and check for left out parts where
+ 		//  MATRIX_DIM is not an exact multiplication of tile size(BLOCK_SIZE)
+ 		if (k*BLOCK_SIZE + threadIdx.x < MATRIX_DIM && Row < MATRIX_DIM){
 
- 		if (k*BLOCK_SIZE + threadIdx.x < MATRIX_DIM && Row < MATRIX_DIM)   As[threadIdx.y][threadIdx.x] = A[Row*MATRIX_DIM + k*BLOCK_SIZE + threadIdx.x];
- 		else                                                   As[threadIdx.y][threadIdx.x] = 0.0;
+ 			As[threadIdx.y][threadIdx.x] = A[Row*MATRIX_DIM + \
+ 				k*BLOCK_SIZE + threadIdx.x];
+ 		}  
+ 		else{
 
- 		if (k*BLOCK_SIZE + threadIdx.y < MATRIX_DIM && Col < MATRIX_DIM)   Bs[threadIdx.y][threadIdx.x] = B[(k*BLOCK_SIZE + threadIdx.y)*MATRIX_DIM + Col];
- 		else                                                   Bs[threadIdx.y][threadIdx.x] = 0.0;
+ 			As[threadIdx.y][threadIdx.x] = 0.0;
+ 		}                                                   
 
+ 		if (k*BLOCK_SIZE + threadIdx.y < MATRIX_DIM && Col < MATRIX_DIM){
+ 			
+ 			Bs[threadIdx.y][threadIdx.x] = B[(k*BLOCK_SIZE + \
+ 				threadIdx.y)*MATRIX_DIM + Col];
+ 		}
+ 		else{
+
+ 			Bs[threadIdx.y][threadIdx.x] = 0.0;
+ 		}                                                   
+
+ 		// Wait till all the threads finish before calculating the results
  		__syncthreads();
 
- 		for (int n = 0; n < BLOCK_SIZE; ++n) CValue += As[threadIdx.y][n] * Bs[n][threadIdx.x];
+ 		for (int n = 0; n < BLOCK_SIZE; ++n) 
+ 			CValue += As[threadIdx.y][n] * Bs[n][threadIdx.x];
 
- 			__syncthreads();
+ 		__syncthreads();
  	}
 
- 	if (Row < MATRIX_DIM && Col < MATRIX_DIM) C[((blockIdx.y * blockDim.y + threadIdx.y)*MATRIX_DIM)+(blockIdx.x*blockDim.x)+threadIdx.x]=CValue;
+ 	// Calculate the result
+ 	if (Row < MATRIX_DIM && Col < MATRIX_DIM) 
+ 		C[((blockIdx.y * blockDim.y + threadIdx.y)*MATRIX_DIM)+\
+ 			(blockIdx.x*blockDim.x)+threadIdx.x]=CValue;
 
  }
 
@@ -226,7 +251,9 @@ static unsigned long inMB(unsigned long bytes)
  		print_usage();
  	}
 
+ 	// Initialize the random seed
  	srand(time(NULL));
+
  	// Create the matrices
  	static Real A[MATRIX_DIM][MATRIX_DIM]; 
  	static Real B[MATRIX_DIM][MATRIX_DIM]; 
@@ -255,17 +282,17 @@ static unsigned long inMB(unsigned long bytes)
 
  		Real* _A;
  		gpuErrchk(cudaMalloc((void**) &_A, matrix_size));
- 		printStats();
+ 		// printStats();
 
  		Real* _B;
  		gpuErrchk(cudaMalloc((void**) &_B, matrix_size));
- 		printStats();
+ 		// printStats();
 
  		Real* _C;
  		gpuErrchk(cudaMalloc((void**) &_C, matrix_size));
- 		printStats();
+ 		// printStats();
 
- 			// copy the matrices to device
+ 		// copy the matrices to device
  		cudaMemcpy(_A, A, matrix_size, cudaMemcpyHostToDevice);
  		cudaMemcpy(_B, B, matrix_size, cudaMemcpyHostToDevice);
 
@@ -273,11 +300,13 @@ static unsigned long inMB(unsigned long bytes)
  		if (argc>2 && 0 == strcmp(argv[2], "-t")){
  			printf("cuda tiled mode\n");
 
+ 			// set the grid and block sizes
  			dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
  			dim3 dimGrid;
  			dimGrid.x = (MATRIX_DIM + dimBlock.x - 1)/dimBlock.x;
  			dimGrid.y = (MATRIX_DIM + dimBlock.y - 1)/dimBlock.y;
 
+ 			// execute the workload in the GPU
  			cuda_tiled_mat_mul<<<dimGrid , dimBlock>>>(_A,_B,_C);
 
  			// Copy back the result
@@ -289,6 +318,7 @@ static unsigned long inMB(unsigned long bytes)
  			// print_matrix(serial_C);
  			// print_matrix(C);
 
+ 			// Compare the reuslts
  			compare_matrices(serial_C,C);
 
  			// free device memory
@@ -300,10 +330,7 @@ static unsigned long inMB(unsigned long bytes)
  		else{
  			printf("cuda mode\n");
 
- 			
-
- 			int K;
- 			K = 100;			
+ 			int K=100;			
  			
  			dim3 threadBlock(BLOCK_SIZE,BLOCK_SIZE);
  			dim3 grid(K,K);
